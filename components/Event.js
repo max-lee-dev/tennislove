fd373a74b02c59a38a0fe4fd97a011fd6e193005
maxlee <maxplee8@gmail.com>
2024-06-17 18:36:50 -0400

    trollface

--- components/Event.js
+++ components/Event.js
@@ -3,11 +3,42 @@ import {Image, StyleSheet} from 'react-native';
 import {View, TextInput, Text, Button, ScrollView, TouchableOpacity} from 'react-native';
 import blankpfp from '../assets/blankpfp.png';
 import {auth, db} from '../Firebase/firebase';
+import loc1 from '../assets/loc1.png';
+import loc2 from '../assets/loc2.png';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import {
-    useFonts,
+  useFonts,
+  LexendDeca_100Thin,
+  LexendDeca_200ExtraLight,
+  LexendDeca_300Light,
+  LexendDeca_400Regular,
+  LexendDeca_500Medium,
+  LexendDeca_600SemiBold,
+  LexendDeca_700Bold,
+  LexendDeca_800ExtraBold,
+  LexendDeca_900Black,
+} from "@expo-google-fonts/lexend-deca";
+import {addDoc, collection, query, where, onSnapshot} from "firebase/firestore";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+
+
+function Event({
+                 navigation,
+                 title,
+                 createdAt,
+                 description,
+                 location,
+                 date,
+                 time,
+                 skill,
+                 creator,
+                 peopleNeeded,
+                 creatorUID,
+                 creatorPfp,
+               }) {
+  let [fontsLoaded] = useFonts({
     LexendDeca_100Thin,
     LexendDeca_200ExtraLight,
     LexendDeca_300Light,
@@ -17,305 +48,294 @@ import {
     LexendDeca_700Bold,
     LexendDeca_800ExtraBold,
     LexendDeca_900Black,
-} from "@expo-google-fonts/lexend-deca";
-import {addDoc, collection, query, where, onSnapshot} from "firebase/firestore";
-import {useCollectionData} from "react-firebase-hooks/firestore";
+  });
+  const strippedDate = date?.substring(0, date.length - 5)
+  if (!fontsLoaded) {
+    return <Text> Loading... </Text>
 
+  }
 
-function Event({
-                   navigation,
-                   title,
-                   createdAt,
-                   description,
-                   location,
-                   date,
-                   time,
-                   skill,
-                   creator,
-                   peopleNeeded,
-                   creatorUID,
-                   creatorPfp,
-               }) {
-    let [fontsLoaded] = useFonts({
-        LexendDeca_100Thin,
-        LexendDeca_200ExtraLight,
-        LexendDeca_300Light,
-        LexendDeca_400Regular,
-        LexendDeca_500Medium,
-        LexendDeca_600SemiBold,
-        LexendDeca_700Bold,
-        LexendDeca_800ExtraBold,
-        LexendDeca_900Black,
-    });
-    const strippedDate = date?.substring(0, date.length - 5)
-    if (!fontsLoaded) {
-        return <Text> Loading... </Text>
+  const chatroomRef = collection(db, "chatrooms");
+  const myChatRooms = query(chatroomRef, where("users", "array-contains", auth.currentUser.uid));
 
+  async function sendRequest() {
+    console.log('send request')
+
+    const uids = [auth.currentUser.uid, creatorUID];
+    const sortedID = uids.sort().join('');
+
+    const newChatroom = {
+      users: [auth.currentUser.uid, creatorUID],
+      messages: [],
+      createdAt: new Date(),
+      roomID: sortedID
     }
 
-    const chatroomRef = collection(db, "chatrooms");
-    const myChatRooms = query(chatroomRef, where("users", "array-contains", auth.currentUser.uid));
+    let loading = true;
+    let hasSingleChatroom = true;
+    let exists = false;
+    console.log('Should be 1')
 
-    async function sendRequest() {
-        console.log('send request')
+    await onSnapshot(myChatRooms, (querySnapshot) => {
+      console.log('Should be 2')
 
-        const uids = [auth.currentUser.uid, creatorUID];
-        const sortedID = uids.sort().join('');
-
-        const newChatroom = {
-            users: [auth.currentUser.uid, creatorUID],
-            messages: [],
-            createdAt: new Date(),
-            roomID: sortedID
-        }
-
-        let loading = true;
-        let hasSingleChatroom = true;
-        let exists = false;
-        console.log('Should be 1')
-
-        await onSnapshot(myChatRooms, (querySnapshot) => {
-            console.log('Should be 2')
-
-            querySnapshot.forEach((doc) => {
-                if (doc.data().roomID === sortedID) {
-                    console.log("chatroom exists, go to chatroom")
-                    navigation?.navigate('Chat Hub Stack', {
-                        screen: 'ChatScreen',
-                        params: {
-                            roomID: sortedID,
-                            otherUID: creatorUID,
-                            pretext: "Hey! I'm interested in your " + location + " event on " + strippedDate + " at " + time + "!"
-                        }
-                    });
-                    exists = true;
-                }
-            });
-            console.log('Should be 3')
-            if (!exists) {
-                console.log("Should be 4: " + exists, loading, hasSingleChatroom)
-
-                console.log("chatroom doesnt exist yet, add")
-                addDoc(chatroomRef, newChatroom).then((docRef) => {
-                    console.log("Document written with ID WTF: ", docRef.id);
-                    navigation.navigate('Chat Hub Stack', {
-                        screen: "ChatScreen",
-                        params: {
-                            roomID: auth.currentUser.uid + creatorUID,
-                            otherUID: creatorUID,
-                            pretext: "Hey! I'm interested in your " + location + " event on " + strippedDate + " at " + time + "!"
-                        }
-                    })
-
-
-                }).catch((error) => {
-                    console.error("Error adding document: ", error);
-                });
-
+      querySnapshot.forEach((doc) => {
+        if (doc.data().roomID === sortedID) {
+          console.log("chatroom exists, go to chatroom")
+          navigation?.navigate('Chat Hub Stack', {
+            screen: 'ChatScreen',
+            params: {
+              roomID: sortedID,
+              otherUID: creatorUID,
+              pretext: "Hey! I'm interested in your " + location + " event on " + strippedDate + " at " + time + "!"
             }
+          });
+          exists = true;
+        }
+      });
+      console.log('Should be 3')
+      if (!exists) {
+        console.log("Should be 4: " + exists, loading, hasSingleChatroom)
+
+        console.log("chatroom doesnt exist yet, add")
+        addDoc(chatroomRef, newChatroom).then((docRef) => {
+          console.log("Document written with ID WTF: ", docRef.id);
+          navigation.navigate('Chat Hub Stack', {
+            screen: "ChatScreen",
+            params: {
+              roomID: auth.currentUser.uid + creatorUID,
+              otherUID: creatorUID,
+              pretext: "Hey! I'm interested in your " + location + " event on " + strippedDate + " at " + time + "!"
+            }
+          })
+
+
+        }).catch((error) => {
+          console.error("Error adding document: ", error);
         });
 
+      }
+    });
 
-    }
 
-    return (
-        <View style={styles.eventContainer}>
-            <View style={styles.HStack}>
+  }
 
-                <View>
-                    <TouchableOpacity style={styles.topLeftView}
-                                      onPress={() => navigation.navigate('Profile', {userID: creatorUID})}>
+  return (
+    <View style={styles.eventContainer}>
+      <View style={styles.HStack}>
 
-                        <View style={{marginRight: 5, marginLeft: 0}}>
+        <View>
+          <TouchableOpacity style={styles.topLeftView}
+                            onPress={() => navigation.navigate('Profile', {userID: creatorUID})}>
 
-                            {!creatorPfp &&
-                                <Image source={blankpfp} style={{
-                                    width: 40, height: 40, marginTop: 5,
-                                    marginRight: 5, borderRadius: 50
-                                }}/>}
-                            {creatorPfp &&
-                                <Image source={{uri: creatorPfp}}
-                                       style={{
-                                           width: 40,
-                                           height: 40,
-                                           marginTop: 5,
-                                           marginRight: 5,
-                                           borderRadius: 50
-                                       }}/>}
-                        </View>
-                        <View style={{paddingTop: 5,}}>
-                            <Text style={{fontFamily: 'LexendDeca_400Regular', fontSize: 18}}>{creator}</Text>
-                            <Text style={styles.font}>{createdAt}</Text>
-                        </View>
-                    </TouchableOpacity>
+            <View style={{marginRight: 5, marginLeft: 0}}>
 
-                </View>
-
-                <View style={styles.topRightView}>
-                    <View style={{paddingRight: 20}}>
-                        <Text style={styles.infoTextPadding}>skill</Text>
-                        <View style={styles.HStack}>
-                            <Icon name={"stats-chart"} size={25} color="black"/>
-                            <Text style={{
-                                fontFamily: 'LexendDeca_300Light',
-                                paddingLeft: 5,
-                                paddingTop: 3,
-                                fontSize: 18,
-
-                            }}>{skill}</Text>
-                        </View>
-
-                    </View>
-                    <View>
-                        <Text style={styles.infoText}>people</Text>
-                        <View style={styles.HStack}>
-                            <Icon name={"people"} size={25} color="black"/>
-                            <Text style={{
-                                fontFamily: 'LexendDeca_300Light',
-                                paddingLeft: 5,
-                                paddingTop: 3,
-                                fontSize: 18,
-
-                            }
-                            }>0/{peopleNeeded}</Text>
-
-                        </View>
-
-                    </View>
-                </View>
+              {!creatorPfp &&
+                <Image source={blankpfp} style={{
+                  width: 40, height: 40, marginTop: 5,
+                  marginRight: 5, borderRadius: 50
+                }}/>}
+              {creatorPfp &&
+                <Image source={{uri: creatorPfp}}
+                       style={{
+                         width: 40,
+                         height: 40,
+                         marginTop: 5,
+                         marginRight: 5,
+                         borderRadius: 50
+                       }}/>}
             </View>
-            <Text style={{fontFamily: "LexendDeca_400Regular", fontSize: 18}}>{title}</Text>
-            <Text style={{fontFamily: "LexendDeca_300Light"}}>{description}</Text>
-            <View style={{backgroundColor: 'transparent', minHeight: '40%'}}>
-
+            <View style={{paddingTop: 5,}}>
+              <Text style={{fontFamily: 'LexendDeca_400Regular', fontSize: 18}}>{creator}</Text>
+              <Text style={styles.font}>{createdAt}</Text>
             </View>
-            <View style={styles.HStack}>
-                <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '75%'}}>
-                    <View style={{paddingRight: 20}}>
-                        <Text style={styles.infoTextPadding}>location</Text>
-                        <View style={styles.HStack}>
-                            <Icon name={"location"} size={25} color="black"/>
-                            <Text numberOfLines={1} style={{
-                                fontFamily: 'LexendDeca_300Light',
-                                paddingLeft: 5,
-                                paddingTop: 3,
-                                fontSize: 18,
-                                maxWidth: '80%',
-
-                            }}>{location}</Text>
-                        </View>
-                    </View>
-                    <View>
-                        <Text style={styles.infoTextPadding}>time</Text>
-                        <View style={styles.HStack}>
-                            <Icon name={"time"} size={25} color="black"/>
-                            <Text style={{
-                                fontFamily: 'LexendDeca_300Light',
-                                paddingLeft: 5,
-                                paddingTop: 3,
-                                fontSize: 18,
-                            }}>{strippedDate} {time}</Text>
-                        </View>
-                    </View>
-
-
-                </View>
-                <TouchableOpacity disabled={creatorUID === auth.currentUser.uid} onPress={() => sendRequest()}
-                                  style={creatorUID === auth.currentUser.uid ? styles.DisabledButton : styles.Button}>
-                    <Icon name={'send'} size={25} color={creatorUID === auth.currentUser.uid ? "gray" : "black"}/>
-                </TouchableOpacity>
-            </View>
-
+          </TouchableOpacity>
 
         </View>
 
-    )
+        <View style={styles.topRightView}>
+          <View style={{paddingRight: 20}}>
+            <Text style={styles.infoTextPadding}>skill</Text>
+            <View style={styles.HStack}>
+              <Icon name={"stats-chart"} size={25} color="black"/>
+              <Text style={{
+                fontFamily: 'LexendDeca_300Light',
+                paddingLeft: 5,
+                paddingTop: 3,
+                fontSize: 18,
+
+              }}>{skill}</Text>
+            </View>
+
+          </View>
+          <View>
+            <Text style={styles.infoText}>people</Text>
+            <View style={styles.HStack}>
+              <Icon name={"people"} size={25} color="black"/>
+              <Text style={{
+                fontFamily: 'LexendDeca_300Light',
+                paddingLeft: 5,
+                paddingTop: 3,
+                fontSize: 18,
+
+              }
+              }>0/{peopleNeeded}</Text>
+
+            </View>
+
+          </View>
+        </View>
+      </View>
+      {/*<Text style={{fontFamily: "LexendDeca_400Regular", fontSize: 18}}>{title}</Text>*/}
+      <Text style={{
+        fontFamily: "LexendDeca_300Light",
+        padding: 5,
+        paddingBottom: 20,
+      }}>{description}</Text>
+      <View style={{backgroundColor: 'transparent', height: "50%", padding: 0}}>
+        {location === "South High School" &&
+          <Image source={loc1} style={{width: '95%', opacity: 0.5, height: '100%', resizeMode: 'cover'}}/>
+        }
+        {location === "Allenwood" &&
+          <Image source={loc2} style={{width: '95%', opacity: 0.5, height: '100%', resizeMode: 'cover'}}/>
+        }
+
+      </View>
+      <View style={styles.HStack}>
+        <View style={{
+          display: 'flex',
+          flexDirection: 'row',
+          paddingTop: 10,
+          justifyContent: 'space-between',
+          width: '75%'
+        }}>
+          <View style={{paddingRight: 20}}>
+            <Text style={styles.infoTextPadding}>location</Text>
+            <View style={styles.HStack}>
+              <Icon name={"location"} size={25} color="black"/>
+              <Text numberOfLines={1} style={{
+                fontFamily: 'LexendDeca_300Light',
+                paddingLeft: 5,
+                paddingTop: 3,
+                fontSize: 18,
+                maxWidth: '80%',
+
+              }}>{location}</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.infoTextPadding}>when</Text>
+            <View style={styles.HStack}>
+              <Icon name={"time"} size={25} color="black"/>
+              <Text style={{
+                fontFamily: 'LexendDeca_300Light',
+                paddingLeft: 5,
+                paddingTop: 3,
+                fontSize: 18,
+              }}>{strippedDate} {time}</Text>
+            </View>
+          </View>
+
+
+        </View>
+        <TouchableOpacity disabled={creatorUID === auth.currentUser.uid} onPress={() => sendRequest()}
+                          style={creatorUID === auth.currentUser.uid ? styles.DisabledButton : styles.Button}>
+          <Icon name={'send'} size={25} color={creatorUID === auth.currentUser.uid ? "gray" : "#fe6446"}/>
+        </TouchableOpacity>
+      </View>
+
+
+    </View>
+
+  )
 }
 
 const styles = StyleSheet.create({
-    eventContainer: {
-        paddingLeft: 20,
-        paddingTop: 5,
-        minWidth: '100%',
-        minHeight: 250,
-        backgroundColor: '#fff',
+  eventContainer: {
+    paddingLeft: 20,
+    paddingTop: 5,
+    minWidth: '100%',
+    minHeight: 340,
+    marginBottom: 20,
+    backgroundColor: '#fff',
 
-    },
+  },
 
-    DisabledButton: {
-        width: "15%",
-        height: 40,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        borderWidth: 1,
-        margin: 5,
-        marginLeft: 20,
-        borderColor: 'gray',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    HStack: {
-        display: 'flex',
-        flexDirection: 'row',
+  DisabledButton: {
+    width: "15%",
+    height: 40,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    margin: 5,
+    marginTop: 20,
 
-    },
-    pfpContainer: {
-        width: 50,
-        height: 50,
-        borderRadius: 50,
-        backgroundColor: '#000',
-        marginRight: 10,
-    },
-    pfp: {
-        width: 50,
-        height: 50,
-        borderRadius: 50,
+    marginLeft: 20,
+    borderColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  HStack: {
+    display: 'flex',
+    flexDirection: 'row',
 
-    },
-    font: {
-        fontFamily: 'LexendDeca_300Light',
-    },
-    infoTextPadding: {
-        alignSelf: 'center',
-        fontFamily: 'LexendDeca_200ExtraLight',
-        color: 'gray',
+  },
+  pfpContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: '#000',
+    marginRight: 10,
+  },
+  pfp: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
 
-    },
+  },
+  font: {
+    fontFamily: 'LexendDeca_300Light',
+  },
+  infoTextPadding: {
+    alignSelf: 'center',
+    fontFamily: 'LexendDeca_200ExtraLight',
+    color: 'gray',
 
-
-    topLeftView: {
-
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
+  },
 
 
-        minWidth: '47%',
+  topLeftView: {
 
-    },
-    Button: {
-        width: "15%",
-        height: 40,
-        backgroundColor: '#fff',
-        borderRadius: 10,
-        borderWidth: 1,
-        margin: 5,
-        marginLeft: 20,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    topRightView: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-        minWidth: '48%',
-    },
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
 
-    infoText: {
-        fontFamily: 'LexendDeca_200ExtraLight',
-        color: 'gray',
 
-    }
+    minWidth: '47%',
+
+  },
+  Button: {
+    width: "15%",
+    height: 40,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    margin: 5,
+    marginTop: 20,
+    marginLeft: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topRightView: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    minWidth: '48%',
+  },
+
+  infoText: {
+    fontFamily: 'LexendDeca_200ExtraLight',
+    color: 'gray',
+
+  }
 
 });
 
--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -40,134 +40,135 @@ const Stack = createStackNavigator();
 
 
 function ChatStack({navigation}) {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name={chatHubName} component={ChatHub} options={{
-                title: 'Chats',
-            }}/>
-            <Stack.Screen name={chatScreenName} component={ChatScreen} options={{
-                headerTitle: 'Messages',
-                headerShown: false,
-            }}/>
-            <Stack.Screen name={profileName} component={Profile}/>
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={chatHubName} component={ChatHub} options={{
+        title: 'Chats',
+      }}/>
+      <Stack.Screen name={chatScreenName} component={ChatScreen} options={{
+        headerTitle: 'Messages',
+        headerShown: false,
+      }}/>
+      <Stack.Screen name={profileName} component={Profile}/>
+    </Stack.Navigator>
+  )
 }
 
 function FeedStack({navigation}) {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name={homeName} component={HomeScreen} options={
-                {
-                    headerRight: () => (
-                        <View>
-                            <Ionicons name={'cog'} size={30} color={'black'} onPress={() => {
-                                navigation.navigate(settingsName)
-                            }
-                            }/>
-                        </View>
-                    ),
-                }
-            }/>
-            <Stack.Screen name={profileName} component={Profile}/>
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={homeName} component={HomeScreen} options={
+        {
+          headerShown: 'true',
+          headerRight: () => (
+            <View>
+              <Ionicons name={'cog'} size={30} color={'black'} onPress={() => {
+                navigation.navigate(settingsName)
+              }
+              }/>
+            </View>
+          ),
+        }
+      }/>
+      <Stack.Screen name={profileName} component={Profile}/>
+    </Stack.Navigator>
+  )
 }
 
 function HomeStack({navigation}) {
-    const [modal, setModal] = React.useState(false);
-    return (
+  const [modal, setModal] = React.useState(false);
+  return (
 
-        <Tab.Navigator
-            screenOptions={({route}) => ({
-                tabBarIcon: ({focused, color, size}) => {
-                    let iconName;
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
 
-                    if (route.name === FeedStackName) {
-                        iconName = focused ? 'home' : 'home-outline';
-                    } else if (route.name === chatHubStackName) {
-                        iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
-                    } else if (route.name === addEventName) {
-                        iconName = focused ? 'add-circle' : 'add-circle-outline';
-                    }
-
-
-                    return <Ionicons name={iconName} size={size} color={color}/>;
-                }
-            })}
-            tabBarOptions={{
-
-                labelStyle: {paddingBottom: 5, fontSize: 10},
-                style: {paddingTop: 10, height: 90},
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray'
-            }}
-        >
-            <Tab.Screen name={FeedStackName} component={FeedStack}
-                        options={{
-                            title: 'Home',
-
-                            headerShown: false,
+          if (route.name === FeedStackName) {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === chatHubStackName) {
+            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+          } else if (route.name === addEventName) {
+            iconName = focused ? 'add-circle' : 'add-circle-outline';
+          }
 
 
-                        }}
-            />
+          return <Ionicons name={iconName} size={size} color={color}/>;
+        }
+      })}
+      tabBarOptions={{
 
-            <Tab.Screen name={addEventName} component={AddEvent}
+        labelStyle: {paddingBottom: 5, fontSize: 10},
+        style: {paddingTop: 10, height: 90},
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray'
+      }}
+    >
+      <Tab.Screen name={FeedStackName} component={FeedStack}
+                  options={{
+                    title: 'Home',
 
-                        options={{
-                            title: 'Add Event',
-                            tabBarLabel: 'Add',
-
-                        }}
-            />
-
-
-            <Tab.Screen name={chatHubStackName} component={ChatStack}
-
-                        options={{
-                            headerShown: false,
-                            title: 'Messages',
-                            tabBarLabel: 'Messages',
-
-                        }}
-            />
+                    headerShown: false,
 
 
-        </Tab.Navigator>
+                  }}
+      />
 
-    )
+      <Tab.Screen name={addEventName} component={AddEvent}
+
+                  options={{
+                    title: 'Add Event',
+                    tabBarLabel: 'Add',
+
+                  }}
+      />
+
+
+      <Tab.Screen name={chatHubStackName} component={ChatStack}
+
+                  options={{
+                    headerShown: false,
+                    title: 'Messages',
+                    tabBarLabel: 'Messages',
+
+                  }}
+      />
+
+
+    </Tab.Navigator>
+
+  )
 }
 
 function LogInStack({navigation}) {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name={loginName} component={LoginScreen} options={{headerShown: false}}/>
-            <Stack.Screen name={signupName} component={SignUpScreen} options={{headerShown: false}}/>
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={loginName} component={LoginScreen} options={{headerShown: false}}/>
+      <Stack.Screen name={signupName} component={SignUpScreen} options={{headerShown: false}}/>
+    </Stack.Navigator>
+  )
 }
 
 export default function MainContainer() {
 
-    return (
-        <NavigationContainer>
+  return (
+    <NavigationContainer>
 
-            <Stack.Navigator
-                tabBarOptions={({route}) => ({
-                        tabBarVisible: false,
-                    }
-                )}
-            >
-
-
-                {!auth.currentUser &&
-                    <Stack.Screen name={loginStackName} component={LogInStack} options={{headerShown: false}}/>}
-                <Stack.Screen name={homeStackName} component={HomeStack} options={{headerShown: false}}/>
-
-            </Stack.Navigator>
+      <Stack.Navigator
+        tabBarOptions={({route}) => ({
+            tabBarVisible: false,
+          }
+        )}
+      >
 
 
-        </NavigationContainer>
-    )
+        {/*{!auth.currentUser &&*/}
+        {/*  <Stack.Screen name={loginStackName} component={LogInStack} options={{headerShown: false}}/>}*/}
+        <Stack.Screen name={homeStackName} component={HomeStack} options={{headerShown: false}}/>
+
+      </Stack.Navigator>
+
+
+    </NavigationContainer>
+  )
 }
